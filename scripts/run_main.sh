9d9f39f179bf90d9a86048a328eab2c7034d04f7
Wei Jin <32392709+ChandlerBang@users.noreply.github.com>
2023-08-19 23:44:59 -0400

    fix file name error in run_main.sh

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -1,17 +1,17 @@
 for r in 0.25 0.5 1
 do
-python train_cond_tranduct.py --dataset cora --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=1 --epoch=600 --save=0
+python train_gcond_transduct.py --dataset cora --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=1 --epoch=600 --save=0
 done
 
 for r in 0.25 0.5 1
 do
-python train_cond_tranduct.py --dataset citeseer --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=1 --epoch=600 --save=0
+python train_gcond_transduct.py --dataset citeseer --nlayers=2 --sgc=1 --lr_feat=1e-4 --gpu_id=0  --lr_adj=1e-4 --r=${r}  --seed=1 --epoch=600 --save=0
 done
 
 
 for r in 0.001 0.005 0.01
 do
-python train_cond_tranduct.py --dataset ogbn-arxiv --nlayers=2 --sgc=1 --lr_feat=0.01 --gpu_id=3  --lr_adj=0.01 --r=${r}  --seed=1 --inner=3  --epochs=1000  --save=0
+python train_gcond_transduct.py --dataset ogbn-arxiv --nlayers=2 --sgc=1 --lr_feat=0.01 --gpu_id=3  --lr_adj=0.01 --r=${r}  --seed=1 --inner=3  --epochs=1000  --save=0
 done
 
 
