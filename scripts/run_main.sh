6832573904773d2a336dadd0a02338b1a6521bd5
Wei Jin <32392709+ChandlerBang@users.noreply.github.com>
2024-05-13 11:47:15 -0400

    Update run_main.sh by adding ratio=0.002

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -20,7 +20,7 @@ do
     python train_gcond_induct.py --dataset flickr --sgc=2 --nlayers=2 --lr_feat=0.005 --lr_adj=0.005  --r=${r} --seed=1 --gpu_id=0 --epochs=1000  --inner=1 --outer=10 --save=0 
 done
 
-for r in 0.001 0.005 0.0005
+for r in 0.001 0.005 0.0005 0.002
 do
     python train_gcond_induct.py --dataset reddit --sgc=1 --nlayers=2 --lr_feat=0.1 --lr_adj=0.1  --r=${r} --seed=1 --gpu_id=0 --epochs=1000  --inner=1 --outer=10 --save=0 
 done
